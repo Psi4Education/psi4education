cbfa65a7854aabe1c47e0c45e1a0e23a7439279c
V.H. Chavez <gonza445@purdue.edu>
2021-03-12 12:31:09 -0500

    Match changes made to instructor version

--- labs/Bond_Breaking/orbital_helper.py
+++ labs/Bond_Breaking/orbital_helper.py
@@ -1,57 +1,28 @@
 
 import psi4 
 import numpy as np
-import matplotlib.pyplot as plt
 
-def order_by_array(x_array, y_array):
+def generate_orbitals(wfn, orbitals):
     """
-    Orders array_y based on x_array
-    """
-    x_out = np.array(x_array)
-    y_out = np.array(y_array)
-    indx = x_out.argsort()
-    x_out = x_out[indx]
-    y_out = y_out[indx]
-    
-    return x_out, y_out
-            
-def points_in_z(x_in_blocks, y_in_blocks, z_in_blocks, function, threshold=1e-15):
-    
-    """
-    Finds the ponits of function that are on the z axis.
+    Generates alpha/beta orbitals on z axis.
     
     Parameters
     ----------
-    x,y,z: List
-        List of numpy arrays with grid blocks
-    function: List
-        List of block of points of a function
-    
+    wfn: psi4.core.Wavefunction
+        Wavefunction from calculation
+    orbitals: List[int], int>0
+        List with requested orbitals
+        
+    Returns
+    -------
+    x: np.ndarray
+        Numpy array of domain correspoding to z axis.
+    orbs_out_a, orbs_out_b: List[np.ndarray]
+        List with requested orbitals on z axis. 
     """
     
-    x_out = []
-    y_out = []
-
-    for block in range(len(x_in_blocks)):                
-        x = x_in_blocks[block]
-        y = y_in_blocks[block]
-        z = z_in_blocks[block]
-
-        for i_point in range(len(x)):
-            if np.abs(x[i_point]) < threshold:
-                if np.abs(y[i_point]) < threshold:
-                    x_out.append(z[i_point])
-                    y_out.append(function[block][i_point])
-
-    x_out, y_out = order_by_array(x_out, y_out)
-    
-    return x_out, y_out
-
-def plot_homo_lumo(wfn):
-    """
-    Plots HOMO/LUMO alpha/beta orbitals for a molecule ordered on z axis.
-
-    """
+    if 0 in orbitals:
+        raise ValueError("Must provide integer values of orbitals. (i_orb > 0)")
 
     #Orbital Matrices
     Ca = wfn.Ca().clone()
@@ -59,7 +30,6 @@ def plot_homo_lumo(wfn):
 
     #Create Vpot object
     lda_functional = psi4.driver.proc.dft.lda_functionals.functional_list["svwn"]
-
     if wfn.same_a_b_orbs() is True:
         functional = psi4.driver.dft.build_superfunctional_from_dictionary(lda_functional, 500000, 1, True)
         functional[0].allocate()
@@ -81,59 +51,42 @@ def plot_homo_lumo(wfn):
     Cb_np = Cb.clone().np
     nbf = len(Ca_np)
 
-    orbitals_a_r = {str(i_orb) : [] for i_orb in range(nbf)}
-    orbitals_b_r = {str(i_orb) : [] for i_orb in range(nbf)}
-    x_in_blocks = []
-    y_in_blocks = []
-    z_in_blocks = []
-
+    a_orbs = { str(i) : np.empty((1,0)) for i in orbitals  }
+    b_orbs = { str(i) : np.empty((1,0)) for i in orbitals  }
+    x_out = np.empty((1,0))
+    
     for b in range(Vpot.nblocks()):
         block = Vpot.get_block(b)
         points_func.compute_points(block)
         npoints = block.npoints()
 
         lpos = np.array(block.functions_local_to_global())
-        w = np.array(block.w())
-
-        x_in_blocks.append(np.array(block.x()))
-        y_in_blocks.append(np.array(block.y()))
-        z_in_blocks.append(np.array(block.z()))
-
+        x, y, z = block.x().np, block.y().np, block.z().np
         phi = np.array(points_func.basis_values()["PHI"])[:npoints, :lpos.shape[0]]
 
         #Range specifies number of orbitals requested
-        for i_orb in range(2):
+        for i_orb in orbitals:
             if len(lpos) != 0:
-                #C_local = C_np[i_orb, lpos]
-                Ca_local = Ca_np[lpos, i_orb]
-                Cb_local = Cb_np[lpos, i_orb]
+                Ca_local = Ca_np[lpos, i_orb-1]
+                Cb_local = Cb_np[lpos, i_orb-1]
                 orb_a = np.einsum('m, pm -> p', Ca_local, phi, optimize=True)
-                orb_b = np.einsum('m, pm -> p', Cb_local, phi, optimize=True)
-                orbitals_a_r[str(i_orb)].append(orb_a)        
-                orbitals_b_r[str(i_orb)].append(orb_b)     
+                orb_b = np.einsum('m, pm -> p', Cb_local, phi, optimize=True)   
+                
+                #Isolate points close to z axis. 
+                mask = np.bitwise_and(np.isclose(x, 0), np.isclose(y, 0))
+                if i_orb == orbitals[0]:
+                    x_out = np.append(x_out, z[mask])
+                a_orbs[ str(i_orb) ] = np.append( a_orbs[str(i_orb)], orb_a[mask] ) 
+                b_orbs[ str(i_orb) ] = np.append( b_orbs[str(i_orb)], orb_b[mask] ) 
+             
+    orbs_out_a = []
+    orbs_out_b = []
+    #Order points according to z
+    indx = np.argsort(x_out)      
+    x_out = x_out[indx]
+    for i in orbitals:
+        orbs_out_a.append( a_orbs[str(i)][indx] )
+        orbs_out_b.append( b_orbs[str(i)][indx] )
 
-    #Order arrays by z
-    x_out, homo_a = points_in_z(x_in_blocks, y_in_blocks, z_in_blocks, orbitals_a_r["0"])
-    _,     homo_b = points_in_z(x_in_blocks, y_in_blocks, z_in_blocks, orbitals_b_r["0"])
-    _,     lumo_a = points_in_z(x_in_blocks, y_in_blocks, z_in_blocks, orbitals_a_r["1"])
-    _,     lumo_b = points_in_z(x_in_blocks, y_in_blocks, z_in_blocks, orbitals_b_r["1"])
-
-    fig, axs = plt.subplots(ncols=2, nrows=2)
-    fig.set_figheight(5)
-    fig.set_figwidth(10)
-    fig.tight_layout()
-
-    p1 = axs[0,0]
-    p2 = axs[0,1]
-    p3 = axs[1,0]
-    p4 = axs[1,1]
-
-    p1.scatter(x_out, homo_a,    c=np.sign(homo_a), cmap="coolwarm", s=3)
-    p2.scatter(x_out, homo_b,    c=np.sign(homo_b), cmap="coolwarm", s=3)
-    p3.scatter(x_out, lumo_a,    c=-np.sign(lumo_a), cmap="coolwarm", s=3)
-    p4.scatter(x_out, lumo_b,    c=-np.sign(lumo_b), cmap="coolwarm", s=3)
-
-    p1.title.set_text("Alpha HOMO")
-    p2.title.set_text("Beta HOMO")
-    p3.title.set_text("Alpha LUMO")
-    p4.title.set_text("Beta LUMO")
+    
+    return x_out, orbs_out_a, orbs_out_b
